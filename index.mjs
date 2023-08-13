b4d2f4bb9fc6826fc84c281bf013fd388ee1b0e1
Apratim Dutta <2021itb089.apratim@students.iiests.ac.in>
2023-08-14 00:03:20 +0530

    initiated

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import express from 'express';
+import whatsappReminder from './whatsappReminder.mjs';
+import schedule from 'node-schedule';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/sendapi', whatsappReminder);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+
+  // Schedule the API request every day at 23:48
+  const scheduledTime = new Date();
+  scheduledTime.setHours(18);
+  scheduledTime.setMinutes(0);
+
+  schedule.scheduleJob(scheduledTime, async () => {
+    console.log('Executing API request at 23:52');
+    try {
+      // Make the HTTP request
+      const response = await axios.get('http://localhost:3000/sendapi/handleContestFetch');
+      console.log('API Response:', response.data.message);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  });
+});
---
+++ whatsappReminder.mjs
@@ -0,0 +1,76 @@
+import express from "express";
+import fetch from "node-fetch";
+import twilio from "twilio";
+
+
+const router = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = twilio(accountSid, authToken);
+
+const findStartTime = (relativeSecondsBefore) => {
+  const currentTime = new Date().getTime();
+  const relativeMilliseconds = relativeSecondsBefore * 1000;
+  const contestStartTime = new Date(currentTime + relativeMilliseconds);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
+  };
+
+  const formattedStartTime = contestStartTime.toLocaleDateString(
+    "en-US",
+    options
+  );
+  return formattedStartTime;
+};
+
+router.get("/handleContestFetch", async (req, res) => {
+  try {
+    const url = "https://codeforces.com/api/contest.list?gym=false";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.result && Array.isArray(data.result)) {
+      data.result.forEach((contest) => {
+        if (contest.phase === "BEFORE") {
+          const contestStartTime = findStartTime(
+            Math.abs(contest.relativeTimeSeconds)
+          );
+          const contestName = contest.name;
+          const duration = contest.durationSeconds / 60;
+
+          const message = `Contest Name: ${contestName}\nContest Start Time: ${contestStartTime}\nContest Duration: ${duration} minutes`;
+
+          client.messages
+            .create({
+              body: message,
+              from: "whatsapp:+14155238886",
+              to: "whatsapp:+919337316233",
+            })
+            .then((message) => console.log(message.sid));
+        }
+      });
+    } else {
+      console.log("Unable to retrieve contest data");
+    }
+
+    res.status(200).json({
+      message: "Contest reminders sent successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "An error occurred",
+    });
+  }
+});
+
+export default router;
